09d3789e84fb8f9dbacd0506861b15f78d1f6e31
VEK <vadimkras@yahoo.com>
2018-09-30 06:55:31 +0000

    local

--- services/webserver-cluster/outputs.tf
+++ services/webserver-cluster/outputs.tf
@@ -6,5 +6,5 @@ output "asg_name"	{
 	value	= "${aws_autoscaling_group.example.name}"
 }
 output "elb_security_group.elb.id" {
-	value = "${aws_security_group.elb.id}"
+	value = "${aws_security_group.web-elb.id}"
 }
